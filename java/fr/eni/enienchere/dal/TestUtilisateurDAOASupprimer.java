81c4796db052f05a9b6476b406d32f3e037bf40c
Cédric Crépin <ccrepin@gmail.com>
2023-05-29 22:53:33 +0200

    Revert "supression du dossier java dans Resources et création dans /src/main/"
    This reverts commit f78d779f7645ec50841313f15a20f22de5fbebc4.

---
+++ java/fr/eni/enienchere/dal/TestUtilisateurDAOASupprimer.java
@@ -0,0 +1,13 @@
+package fr.eni.enienchere.dal;
+
+public class TestUtilisateurDAOASupprimer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		UtilisateurDAOJdbcImpl teste = new UtilisateurDAOJdbcImpl();
+		teste.select();
+
+	}
+
+}
---
+++ java/fr/eni/enienchere/dal/UtilisateurDAO.java
@@ -0,0 +1,12 @@
+package fr.eni.enienchere.dal;
+
+import fr.eni.enienchere.bo.Utilisateur;
+
+public interface UtilisateurDAO {
+	
+	public Utilisateur select(); //throws BusinessException;
+	
+	
+
+}
+
