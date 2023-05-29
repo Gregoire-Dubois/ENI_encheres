172f8e7343804260c6cbedec0f2ea97a77552713
Priscillia Popieul <p.popieul@laposte.net>
2023-05-29 23:51:40 +0200

    Correction erreur URL

--- src/main/java/fr/eni/enienchere/ServletTestPoolConnexion.java
+++ src/main/java/fr/eni/enienchere/ServletTestPoolConnexion.java
@@ -19,7 +19,7 @@ import javax.sql.DataSource;
 /**
  * Servlet implementation class ServletTestPoolConnexion
  */
-@WebServlet("/eni_enchere/ServletTestPoolConnexion")
+@WebServlet("/eni_encheres/ServletTestPoolConnexion")
 public class ServletTestPoolConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
