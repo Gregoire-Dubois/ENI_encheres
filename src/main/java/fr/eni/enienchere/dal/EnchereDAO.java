14e284e2c58e4675991dce72e73a6f460c05a9b5
Cédric Crépin <ccrepin@gmail.com>
2023-05-31 09:04:18 +0200

    Premiers jets pour l'objet Enchere / Non finalisé

---
+++ src/main/java/fr/eni/enienchere/dal/EnchereDAO.java
@@ -0,0 +1,21 @@
+package fr.eni.enienchere.dal;
+
+import java.util.List;
+
+import fr.eni.enienchere.BusinessException;
+import fr.eni.enienchere.bo.Enchere;
+
+public interface EnchereDAO {
+	//Liste de toutes les enchères
+	List<Enchere> selectAllEnchere() throws BusinessException;
+	
+	//Liste des enchères d'un utilisateur
+	
+	//Liste des enchères selon article
+	
+	//Ajout d'une enchère 
+	
+	//Sélection d'une enchère selon id
+	
+	//
+}
