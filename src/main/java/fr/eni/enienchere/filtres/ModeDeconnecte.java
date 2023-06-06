9dee5e3c0f4d238138ba8cd2ca5f2e180e226f70
Priscillia Popieul <p.popieul@laposte.net>
2023-06-06 22:27:48 +0200

    Mise en place du filtre de redirection quand on est deconnecté

---
+++ src/main/java/fr/eni/enienchere/filtres/ModeDeconnecte.java
@@ -0,0 +1,70 @@
+package fr.eni.enienchere.filtres;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class ModeDeconnecte
+ */
+@WebFilter(dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD, 
+				DispatcherType.INCLUDE, 
+				DispatcherType.ERROR
+		}
+					, urlPatterns = { "/*" })
+public class ModeDeconnecte implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public ModeDeconnecte() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			HttpSession session = httpRequest.getSession();
+			if(session.getAttribute("userConnected")!=null) {
+				chain.doFilter(request, response);
+			}else {
+				if (!(httpRequest.getServletPath().toLowerCase().contains("accueil")||httpRequest.getServletPath().toLowerCase().contains("connexion")||httpRequest.getServletPath().toLowerCase().contains("inscription"))){
+					//System.out.println(httpRequest.getServletPath().toLowerCase());
+					HttpServletResponse httpResponse = (HttpServletResponse) response;
+					httpResponse.sendRedirect(httpRequest.getContextPath()+"/accueil");	
+					
+					return;
+				}
+			}
+			chain.doFilter(request, response);
+		}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
