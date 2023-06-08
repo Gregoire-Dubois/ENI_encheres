c78528057d82b1d80a398d57c40b7d199d5e7193
Cédric Crépin <ccrepin@gmail.com>
2023-06-08 16:13:26 +0200

    Correction sur le contrôle du crédit et affichage des messages

--- src/main/java/fr/eni/enienchere/servlet/CodesResultatServlet.java
+++ src/main/java/fr/eni/enienchere/servlet/CodesResultatServlet.java
@@ -3,4 +3,7 @@ package fr.eni.enienchere.servlet;
 public abstract class CodesResultatServlet {
 	public static final int MDP_NON_IDENTIQUE_ECHEC = 30000;
 	public static final int PAS_D_ARTICLES_ERREUR = 30001;
+	public static final int CREDIT_INFERIEUR_PRIX_VENTE = 30002;
+	public static final int ENCHERE_INFERIEURE_PRIX_VENTE = 30003;
+	public static final int ENCHERE_IMPOSSIBLE = 30004;
 }
